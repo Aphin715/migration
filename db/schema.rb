15304962f315ffb1786038b772fa6d04332affc4
Alex Phinizy <AlexPhinizy@gmail.com>
2013-09-06 19:07:14 -0400

    final

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130906205158) do
+ActiveRecord::Schema.define(version: 20130906230431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,27 @@ ActiveRecord::Schema.define(version: 20130906205158) do
     t.datetime "updated_at"
     t.string   "author"
     t.integer  "rating"
+  end
+
+  create_table "books_categories", id: false, force: true do |t|
+    t.integer "book_id",     null: false
+    t.integer "category_id", null: false
+  end
+
+  add_index "books_categories", ["book_id"], name: "index_books_categories_on_book_id", using: :btree
+  add_index "books_categories", ["category_id"], name: "index_books_categories_on_category_id", using: :btree
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "checkouts", force: true do |t|
+    t.integer  "book_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+book_attributes = [
+  {
+    title: 'Grapes of Wrath',
+    author: 'Hemmingway',
+    rating: 5
+  },
+  {
+    title: 'game of thrones',
+    author: 'Hemmingway',
+    rating: 5
+  },
+  {
+    title: 'boo ya baby',
+    author: 'Hemmingway',
+    rating: 5
+  }
+]
+
+
+
+
+book_attributes.each do |page|
+
+
+  Book.find_or_create_by_title_and_author(title: page[:title],author: page[:author]) do |book|
+    book.rating = page[:rating]
+  end
+end
+
+cats = ['science fiction', 'horror', 'ficton', 'non-fiction']
+
+  cats.each do |cat|
+    Category.find_or_create_by_name(cat)
+end
+
+
+
+# book = Book.where(title: title).first?
+
+# if book.nil?
+#   Book.create(book_attributes)
+# end
