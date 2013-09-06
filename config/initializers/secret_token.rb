45ba2dd96e0d58ee444d719fc30ccb75a5d4ba1f
Alex Phinizy <AlexPhinizy@gmail.com>
2013-09-06 16:56:08 -0400

    step 5 done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Library::Application.config.secret_key_base = 'bef3fa98895fda794b6318eafa8a85100106e2deb3143fba45bad75aa5d877b193d018685d79cf578b9cbee66d59772231536de837345eafd37a2031277112b2'
---
+++ db/migrate/20130906183246_add_book_rating.rb
@@ -0,0 +1,9 @@
+class AddBookRating < ActiveRecord::Migration
+  def up
+    add_column :books, :rating, :integer
+  end
+
+  def down
+    remove_column :books, :rating
+  end
+end
